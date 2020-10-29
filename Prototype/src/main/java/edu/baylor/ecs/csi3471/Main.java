befa7b4a6fc70c3b5625a852dfe89412b176fb4d
Owen Murphy <owen261@icloud.com>
2020-10-28 23:20:35 -0500

    more mods to windows

--- Prototype/src/main/java/edu/baylor/ecs/csi3471/Main.java
+++ Prototype/src/main/java/edu/baylor/ecs/csi3471/Main.java
@@ -1,7 +1,12 @@
 package edu.baylor.ecs.csi3471;
 
+import java.io.IOException;
+import java.util.logging.Logger;
+
 public class Main {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
+        MySetup.setup();
         Window.CreateWindow();
     }
 }
