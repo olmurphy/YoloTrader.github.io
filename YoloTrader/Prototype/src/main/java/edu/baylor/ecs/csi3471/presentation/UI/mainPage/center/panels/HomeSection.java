85545d6ded3a6a1c9689cc8df4b404723af45fa0
Kal-Abali <71574006+Kal-Abali@users.noreply.github.com>
2020-11-28 18:23:27 -0600

    Bug Fixes

--- YoloTrader/Prototype/src/main/java/edu/baylor/ecs/csi3471/presentation/UI/mainPage/center/panels/HomeSection.java
+++ YoloTrader/Prototype/src/main/java/edu/baylor/ecs/csi3471/presentation/UI/mainPage/center/panels/HomeSection.java
@@ -16,6 +16,7 @@ import edu.baylor.ecs.csi3471.presentation.UI.stockPage.NewsPanel;
 
 public class HomeSection {
 
+	
     public static JPanel homeMainPanel;
 
     public static JPanel getHomeMainPanel() {
@@ -24,13 +25,14 @@ public class HomeSection {
 
     public static void setHomeMainPanel() {
         HomeSection.homeMainPanel = new JPanel(new FlowLayout());
+        HomeSection.homeMainPanel.setBackground(Color.BLACK);
 
         //Set up the top winners
         HomeSection.homeMainPanel.setBackground(Color.BLACK);
         JLabel todaysWins = new JLabel();
         todaysWins.setForeground(Color.WHITE);
        
-        todaysWins.setText("Today's Best Performing Stocks");
+        todaysWins.setText("[ Today's Best Performing Stocks ]");
         todaysWins.setFont(new Font("Futura", Font.PLAIN, 22));
         todaysWins.setBorder(new EmptyBorder(40, 0, 20, 0));
         JScrollPane winners = StockUtil.getTopWinners().getFeed();
@@ -42,15 +44,15 @@ public class HomeSection {
         
         green.add(todaysWins, BorderLayout.NORTH);
         green.add(winners, BorderLayout.SOUTH);
-
-
-
-
-
+        
+        
+        
+        
+        
         //Set up the top losers
         JLabel todaysloss = new JLabel();
         todaysloss.setForeground(Color.WHITE);
-        todaysloss.setText("Today's Worst Performing Stocks");
+        todaysloss.setText("[ Today's Worst Performing Stocks ]");
         todaysloss.setFont(new Font("Futura", Font.PLAIN, 22));
         todaysloss.setBorder(new EmptyBorder(40, 0, 20, 0));
         JScrollPane losers = StockUtil.getTopLosers().getFeed();
@@ -59,8 +61,8 @@ public class HomeSection {
         temp.setLayout(new FlowLayout(FlowLayout.RIGHT));
         temp.add(todaysloss);
         temp.setBackground(Color.BLACK);
-
-
+        
+        
         JPanel red = new JPanel();
         red.setLayout(new BoxLayout(red, BoxLayout.PAGE_AXIS));
         red.setBackground(Color.BLACK);
@@ -68,25 +70,23 @@ public class HomeSection {
         red.add(temp);
         red.add(losers);
         red.setBorder(new EmptyBorder(15,0,15,0));
-
-
+        
+        
         
         //Set up the newsFeed.
-
-		NewsPanel news = StockUtil.getGeneralNews();
+       
+	JScrollPane news = StockUtil.getGeneralNews().getNewsScrollPane();
+	JPanel newsfeed = new JPanel();
+	newsfeed.setPreferredSize(new Dimension(1200, 300));
+	newsfeed.setBackground(Color.BLACK);
+	newsfeed.setLayout(new BoxLayout(newsfeed, BoxLayout.PAGE_AXIS));
+	newsfeed.add(news);
 		
-		HomeSection.homeMainPanel.add(green, BorderLayout.NORTH);
-		HomeSection.homeMainPanel.add(red, BorderLayout.CENTER);
-      HomeSection.homeMainPanel.add(news.getJPanel(), BorderLayout.SOUTH);
-
-
-
-
-
-
-
-
-
-
+		
+	HomeSection.homeMainPanel.add(green, BorderLayout.NORTH);
+	HomeSection.homeMainPanel.add(red, BorderLayout.CENTER);
+	HomeSection.homeMainPanel.add(newsfeed, BorderLayout.PAGE_END);
+        
+       
     }
 }
